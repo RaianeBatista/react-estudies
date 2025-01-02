f157a0ecb39560f152bbf0954a4f57b25bd19e53
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-02 13:23:04 -0300

    feat add alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,13 @@ const Page = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <button
-        onClick={() => alert('Funcionou')}
+        onClick={() => alert('Funcionou 1')}
+        className='p-3 bg-blue-700 text-white rounded-md mr-3'
+      >
+        Clique aqui
+      </button>
+      <button
+        onClick={() => alert('Funcionou 2')}
         className='p-3 bg-blue-700 text-white rounded-md'
       >
         Clique aqui
