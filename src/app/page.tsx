a722bcf5885e4706528aeba0244eb11afba8317e
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-02 20:22:56 -0300

    feat: add hook useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import { FormEvent } from "react";
+import { FormEvent } from 'react';
 
 const Page = () => {
-
-const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     alert('Formulário enviado com sucesso!');
   };
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
       <h1 className='text-5xl mb-4'>Form de Login</h1>
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <input type='text' />
         <input type='submit' value='Enviar' />
       </form>
