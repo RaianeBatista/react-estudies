cb12c081c192d149c392de4e4feee4cd7521478a
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-02 16:07:46 -0300

    feat: add

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -5,7 +5,9 @@ type Props = {
 
 export const CustomButton = ({ label, onClick }: Props) => {
     const handleButtonClick = () => {
+        //alguma coisa
         onClick();
+        //depois de alguma coisa
     }
   return (
     <button onClick={handleButtonClick} className='p-3 text-white bg-blue-700 rounded-md'>{label}</button>
