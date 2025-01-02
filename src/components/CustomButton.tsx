e74213a563bd160b382a834186c39c30facf25da
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-02 15:50:10 -0300

    feat: add a função handleButtonClinck no componente

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,7 +4,10 @@ type Props = {
 };
 
 export const CustomButton = ({ label, onClick }: Props) => {
+    const handleButtonClick = () => {
+        onClick();
+    }
   return (
-    <button onClick={onClick} className='p-3 text-white bg-blue-700 rounded-md'>{label}</button>
+    <button onClick={handleButtonClick} className='p-3 text-white bg-blue-700 rounded-md'>{label}</button>
   );
 };
